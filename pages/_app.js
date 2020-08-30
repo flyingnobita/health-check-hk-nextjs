63363f140b59f4ca61364a5494e46091717312cc
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2020-08-30 14:28:16 +0800

    add css to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import "../styles/index.css";
+import "../styles/App.css";
+import "../styles/pivottable.css";
+import "../styles/ReactPivotTable.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
