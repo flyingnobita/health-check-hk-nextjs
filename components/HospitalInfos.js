4705d9238f4049f69b5c926225e0f741c21cf88f
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2020-09-03 18:25:12 +0800

    show hospitalInfos as cards

--- components/HospitalInfos.js
+++ components/HospitalInfos.js
@@ -1,48 +1,12 @@
 import { Grid } from "@material-ui/core";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
 import React from "react";
 import Emoji from "./Emoji";
+import HospitalInfosCards from "./HospitalInfosCards";
+import HospitalInfosTable from "./HospitalInfosTable";
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    // backgroundColor: theme.palette.black,
-    // color: theme.palette.common.white,
-    fontWeight: "bold",
-  },
-  body: {
-    fontSize: 14,
-    padding: 10,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.grey[50],
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-  hospitalInfoGrid: {
-    paddingTop: "20px",
-    paddingBottom: "5px",
-  },
-});
-
-const onlineBookingLink = function (language, row) {
+export const onlineBookingLink = function (language, row) {
   if (row.booking) {
     if (language === "en") {
       return (
@@ -69,116 +33,16 @@ const onlineBookingLink = function (language, row) {
         </Link>
       );
     }
-  }
+  } else return null;
 };
 
 export default function HospitalInfos({ hospitalInfo, language }) {
-  const classes = useStyles();
-
   if (hospitalInfo.length > 0) {
     return (
-      <Grid container>
-        <Grid item xs={1} className={classes.hospitalInfoGrid} />
-        <Grid item xs={10} className={classes.hospitalInfoGrid}>
-          <TableContainer component={Paper}>
-            <Table
-              className={classes.table}
-              size="small"
-              aria-label="Hospital Infomation"
-            >
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>
-                    {language === "en" ? "Hospital" : "醫院"}
-                  </StyledTableCell>
-                  <StyledTableCell align="left">
-                    {language === "en" ? "🕗 Operating Hours" : "🕗 開放時間"}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {language === "en" ? "📞 Telephone" : "📞 電話"}
-                  </StyledTableCell>
-                  <StyledTableCell align="left">
-                    {language === "en" ? "📍 Address" : "📍 地址"}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {language === "en" ? "📅 Online Booking" : "📅 網上預約"}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {language === "en" ? "🌐 Website" : "🌐 網頁"}
-                  </StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {hospitalInfo.map((row) => (
-                  <StyledTableRow key={row["hospital"]}>
-                    <StyledTableCell component="th" scope="row">
-                      {language === "en" ? row.hospital : row.hospitalCN}
-                    </StyledTableCell>
-                    <StyledTableCell align="left">
-                      {language === "en"
-                        ? row.hospitalHours
-                        : row.hospitalHoursCN}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.telephone}
-                    </StyledTableCell>
-                    <StyledTableCell align="left">
-                      {language === "en" ? (
-                        <Link
-                          target="_blank"
-                          href={row.addressLink}
-                          underline="none"
-                          id={row.hospital}
-                          className="Address"
-                        >
-                          {row.address ? row.address : ""}
-                        </Link>
-                      ) : (
-                        <Link
-                          target="_blank"
-                          href={row.addressLink}
-                          underline="none"
-                          id={row.hospital}
-                          className="Address"
-                        >
-                          {row.addressCN ? row.addressCN : ""}
-                        </Link>
-                      )}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {onlineBookingLink(language, row)}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {language === "en" ? (
-                        <Link
-                          target="_blank"
-                          href={row.website}
-                          underline="none"
-                          id={row.hospital}
-                          className="Website"
-                        >
-                          <Emoji symbol="🔗" label="link" />
-                        </Link>
-                      ) : (
-                        <Link
-                          target="_blank"
-                          href={row.websiteCN}
-                          underline="none"
-                          id={row.hospital}
-                          className="Website"
-                        >
-                          <Emoji symbol="🔗" label="link" />
-                        </Link>
-                      )}
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-        <Grid item xs={1} className={classes.hospitalInfoGrid} />
-      </Grid>
+      <React.Fragment>
+        <HospitalInfosCards language={language} hospitalInfo={hospitalInfo} />
+        {/* <HospitalInfosTable language={language} hospitalInfo={hospitalInfo} /> */}
+      </React.Fragment>
     );
   } else {
     return (
---
+++ components/HospitalInfosCards.js
@@ -0,0 +1,199 @@
+import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import Emoji from "./Emoji";
+import { onlineBookingLink } from "./HospitalInfos";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    maxWidth: 400,
+    width: 240,
+    minHeight: 220,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  cardContent: {
+    paddingTop: 8,
+    paddingBottom: 4,
+  },
+  cardTelephone: {
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  cardTelephoneNumber: {
+    marginLeft: 4,
+  },
+  cardAddress: {
+    marginBottom: 8,
+  },
+  cardAddressText: {
+    marginLeft: 4,
+  },
+  cardHours: {
+    marginBottom: 4,
+  },
+  cardHoursText: {
+    marginLeft: 4,
+  },
+  cardActions: {
+    paddingTop: 0,
+    paddingBottom: 8,
+  },
+});
+
+export default function HospitalInfosCards({ language, hospitalInfo }) {
+  const classes = useStyles();
+
+  return (
+    // page wide container
+    <Grid container>
+      <Grid item xs={1} className={classes.hospitalInfoGrid} />
+      <Grid item xs={10} className={classes.hospitalInfoGrid}>
+        {/* container for all cards */}
+        <Grid container spacing={3}>
+          {hospitalInfo.map((row) => (
+            <Grid item key={row.hospital}>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="h6" component="h2">
+                    {language === "en" ? row.hospital : row.hospitalCN}
+                  </Typography>
+                  {/* container for icon & content */}
+                  <Grid container>
+                    <Grid item xs={1} align="center">
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={classes.cardTelephone}
+                      >
+                        📞
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={11}>
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={`${classes.cardTelephone} ${classes.cardTelephoneNumber}`}
+                      >
+                        {row.telephone}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  {/* container for icon & content */}
+                  <Grid container>
+                    <Grid item xs={1} align="center">
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={classes.cardAddress}
+                      >
+                        📍
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={11}>
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        className={`${classes.cardAddress} ${classes.cardAddressText}`}
+                      >
+                        {language === "en" ? (
+                          <Link
+                            target="_blank"
+                            href={row.addressLink}
+                            underline="none"
+                            id={row.hospital}
+                            className="Address"
+                          >
+                            {row.address ? row.address : ""}
+                          </Link>
+                        ) : (
+                          <Link
+                            target="_blank"
+                            href={row.addressLink}
+                            underline="none"
+                            id={row.hospital}
+                            className="Address"
+                          >
+                            {row.addressCN ? row.addressCN : ""}
+                          </Link>
+                        )}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  {/* container for icon & content */}
+                  <Grid container>
+                    <Grid item xs={1} align="center">
+                      <Typography
+                        variant="body2"
+                        component="span"
+                        className={classes.cardHours}
+                      >
+                        🕗
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={11}>
+                      <Typography
+                        variant="body2"
+                        component="div"
+                        className={`${classes.cardHours} ${classes.cardHoursText}`}
+                      >
+                        {language === "en"
+                          ? row.hospitalHours.split(", ").map((i, key) => {
+                              return <div key={key}>{i}</div>;
+                            })
+                          : row.hospitalHoursCN.split(", ").map((i, key) => {
+                              return <div key={key}>{i}</div>;
+                            })}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+
+                <CardActions className={classes.cardActions}>
+                  <Grid container justify="space-around">
+                    <Grid item>
+                      {language === "en" ? "📅 Booking: " : "📅 網上預約: "}
+                      {onlineBookingLink(language, row)}
+                    </Grid>
+                    <Grid item>
+                      {language === "en" ? "🌐 Website: " : "🌐 網頁: "}
+                      {language === "en" ? (
+                        <Link
+                          target="_blank"
+                          href={row.website}
+                          underline="none"
+                          id={row.hospital}
+                          className="Website"
+                        >
+                          <Emoji symbol="🔗" label="link" />
+                        </Link>
+                      ) : (
+                        <Link
+                          target="_blank"
+                          href={row.websiteCN}
+                          underline="none"
+                          id={row.hospital}
+                          className="Website"
+                        >
+                          <Emoji symbol="🔗" label="link" />
+                        </Link>
+                      )}
+                    </Grid>
+                  </Grid>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={1} className={classes.hospitalInfoGrid} />
+    </Grid>
+  );
+}
